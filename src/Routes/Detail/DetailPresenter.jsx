7987653e0165e728de9a26453e9e8941a84b752d
Yunsu <kenatman0723@gmail.com>
2021-10-13 18:25:18 +0900

    Detail component pt.2 : rendering info

--- src/Routes/Detail/DetailPresenter.jsx
+++ src/Routes/Detail/DetailPresenter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Loader from "../../Components/Loader";
+import Message from "../../Components/Message";
 
 const Container = styled.div`
   height: calc(100vh - 50px);
@@ -41,23 +42,88 @@ const Cover = styled.div`
   background-position: center;
 `;
 
+const Data = styled.div`
+  width: 70%;
+  margin-left: 30px;
+  padding-top: 10px;
+`;
+
+const Title = styled.h2`
+  font-size: 32px;
+`;
+
+const ItemContainer = styled.div`
+  margin: 20px 0;
+  font-size: 20px;
+  width: 70%;
+`;
+
+const Item = styled.span`
+  line-height: 1.4;
+`;
+
+const Divider = styled.span`
+  margin: 0 10px;
+`;
+
+const Overview = styled.p`
+  font-size: 16px;
+  width: 70%;
+  line-height: 1.4;
+  opacity: 0.8;
+`;
+
 const DetailPresenter = ({ result, error, loading }) => {
   return loading ? (
     <Loader />
   ) : (
     <Container>
-      <Backdrop
-        bgImage={`https://image.tmdb.org/t/p/original/${result.backdrop_path}`}
-      />
-      <Content>
-        <Cover
-          bgImage={
-            result.poster_path
-              ? `https://image.tmdb.org/t/p/original/${result.poster_path}`
-              : require("../../images/no-image.jpeg").default
-          }
-        />
-      </Content>
+      {result && (
+        <>
+          <Backdrop
+            bgImage={`https://image.tmdb.org/t/p/original/${result.backdrop_path}`}
+          />
+          <Content>
+            <Cover
+              bgImage={
+                result.poster_path
+                  ? `https://image.tmdb.org/t/p/original/${result.poster_path}`
+                  : require("../../images/no-image.jpeg").default
+              }
+            />
+            <Data>
+              <Title>
+                {result.original_title
+                  ? result.original_title
+                  : result.original_name}
+              </Title>
+              <ItemContainer>
+                <Item>
+                  {result.release_date
+                    ? result.release_date.substring(0, 4)
+                    : result.first_air_date.substring(0, 4)}
+                </Item>
+                <Divider>🌟</Divider>
+                <Item>
+                  {result.runtime ? result.runtime : result.episode_run_time[0]}{" "}
+                  min
+                </Item>
+                <Divider>🌟</Divider>
+                <Item>
+                  {result.genres &&
+                    result.genres.map((genre, index) =>
+                      index === result.genres.length - 1
+                        ? genre.name
+                        : `${genre.name} / `
+                    )}
+                </Item>
+              </ItemContainer>
+              <Overview>{result.overview}</Overview>
+            </Data>
+          </Content>
+        </>
+      )}
+      {error && <Message text={error} color="#e84393" />}
     </Container>
   );
 };
