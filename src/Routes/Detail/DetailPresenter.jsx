8e7d7f01976dbc9541745422fae01dd9a7017561
Yunsu <kenatman0723@gmail.com>
2021-10-27 15:06:58 +0900

    BrowserRouter.. refresh

--- src/Routes/Detail/DetailPresenter.jsx
+++ src/Routes/Detail/DetailPresenter.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Helmet from "react-helmet";
-import { Link } from "react-router-dom";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
 
